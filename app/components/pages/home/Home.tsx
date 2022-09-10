2004fa3f18a11da03d3931186d27cc1055cee0f0
krutulkamil <krutulkamil@onet.pl>
2022-09-10 19:03:30 +0200

    _document.tsx file

--- app/components/pages/home/Home.tsx
+++ app/components/pages/home/Home.tsx
@@ -1,8 +1,14 @@
 import { FunctionComponent } from "react";
+import Layout from "@/components/layout/Layout";
+import Discover from "@/components/pages/home/discover/Discover";
+import Catalog from "@/components/pages/home/catalog/Catalog";
 
 const Home: FunctionComponent = () => {
     return (
-        <div>HOME</div>
+        <Layout title="Videos | Home Page">
+            <Discover />
+            <Catalog />
+        </Layout>
     );
 };
 
