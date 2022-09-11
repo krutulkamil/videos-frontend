14f954a8d29a4fbcf19dbe507477cd1925957621
krutulkamil <krutulkamil@onet.pl>
2022-09-11 11:17:02 +0200

    presentational TextArea component

---
+++ app/components/ui/text-area/TextArea.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent, forwardRef } from "react";
+import styles from "./TextArea.module.scss";
+import { ITextArea } from "@/components/ui/text-area/text-area.interface";
+
+const TextArea: FunctionComponent = forwardRef<HTMLTextAreaElement, ITextArea>(
+    ({ error, style, ...rest }, ref) => {
+        return (
+            <div className={styles["editor"]} style={style}>
+                <textarea ref={ref} {...rest}>
+                    {error && <div className={styles.error}>{error.message}</div>}
+                </textarea>
+            </div>
+        );
+    }
+);
+
+TextArea.displayName = "TextArea";
+
+export default TextArea;
