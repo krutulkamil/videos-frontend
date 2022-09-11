552a6cae8f6b80bb584491227729a835af60a731
krutulkamil <krutulkamil@onet.pl>
2022-09-11 10:45:30 +0200

    useOutside & AuthForm init

--- app/components/layout/header/auth-form/AuthForm.tsx
+++ app/components/layout/header/auth-form/AuthForm.tsx
@@ -1,9 +1,38 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import styles from "./AuthForm.module.scss";
+import stylesIcon from "../icons-right/IconsRight.module.scss";
+import { useOutside } from "../../../../../hooks/useOutside";
+import { useAuth } from "../../../../../hooks/useAuth";
+import { IAuthFields } from "@/components/layout/header/auth-form/auth-form.interface";
+import { FaUserCircle } from "react-icons/fa";
 
 const AuthForm: FunctionComponent = () => {
+    const { ref, isShow, setIsShow } = useOutside(false);
+    const [type, setType] = useState<"login" | "register">("login");
+
+    // const { isLoading } = useAuth();
+
+    const { register, formState: { errors }, handleSubmit } = useForm<IAuthFields>({
+        mode: "onChange"
+    });
+
+    const onSubmit: SubmitHandler<IAuthFields> = data => {
+
+    };
+
     return (
-        <div>Auth Form</div>
+        <div className={styles.wrapper} ref={ref}>
+            <button className={stylesIcon.button} onClick={() => setIsShow(!isShow)}>
+                <FaUserCircle fill="#A4A4A4" />
+            </button>
+
+            {isShow && (
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+
+                </form>
+            )}
+        </div>
     );
 };
 
