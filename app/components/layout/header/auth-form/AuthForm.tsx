52718f1ab2e32ffb39ce0067f950ba89c5045c49
krutulkamil <krutulkamil@onet.pl>
2022-09-28 19:02:10 +0200

    AuthForm - connect with redux actions

--- app/components/layout/header/auth-form/AuthForm.tsx
+++ app/components/layout/header/auth-form/AuthForm.tsx
@@ -1,54 +1,58 @@
-import { FunctionComponent, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { FaUserCircle } from "react-icons/fa";
-import Field from "@/components/ui/field/Field";
-import Button from "@/components/ui/button/Button";
-import styles from "./AuthForm.module.scss";
-import stylesIcon from "../icons-right/IconsRight.module.scss";
-import { useOutside } from "../../../../../hooks/useOutside";
-import { useAuth } from "../../../../../hooks/useAuth";
-import { IAuthFields } from "@/components/layout/header/auth-form/auth-form.interface";
-import { validEmail } from "@/components/layout/header/auth-form/auth.valid";
+import { FunctionComponent, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { FaUserCircle } from 'react-icons/fa';
+import Field from '@/components/ui/field/Field';
+import Button from '@/components/ui/button/Button';
+import styles from './AuthForm.module.scss';
+import stylesIcon from '../icons-right/IconsRight.module.scss';
+import { useOutside } from '../../../../../hooks/useOutside';
+import { useAuth } from '../../../../../hooks/useAuth';
+import { IAuthFields } from '@/components/layout/header/auth-form/auth-form.interface';
+import { validEmail } from '@/components/layout/header/auth-form/auth.valid';
+import { useActions } from '../../../../../hooks/useActions';
 
 const AuthForm: FunctionComponent = () => {
     const { ref, isShow, setIsShow } = useOutside(false);
-    const [type, setType] = useState<"login" | "register">("login");
+    const [type, setType] = useState<'login' | 'register'>('login');
 
-    // const { isLoading } = useAuth();
+    const { login, register: registerAction } = useActions();
+
+    const { isLoading } = useAuth();
 
     const { register, formState: { errors }, handleSubmit } = useForm<IAuthFields>({
-        mode: "onChange"
+        mode: 'onChange'
     });
 
     const onSubmit: SubmitHandler<IAuthFields> = data => {
-
+        if (type === 'login') login(data);
+        else if (type === 'register') registerAction(data);
     };
 
     return (
         <div className={styles.wrapper} ref={ref}>
             <button className={stylesIcon.button} onClick={() => setIsShow(!isShow)}>
-                <FaUserCircle fill="#A4A4A4" />
+                <FaUserCircle fill="#A4A4A4"/>
             </button>
 
             {isShow && (
                 <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                     <Field
-                        {...register("email", {
-                            required: "E-mail is required!",
+                        {...register('email', {
+                            required: 'E-mail is required!',
                             pattern: {
                                 value: validEmail,
-                                message: "Invalid e-mail address!"
+                                message: 'Invalid e-mail address!'
                             }
                         })}
                         placeholder="E-mail"
                         error={errors.email}
                     />
                     <Field
-                        {...register("password", {
-                            required: "Password is required!",
+                        {...register('password', {
+                            required: 'Password is required!',
                             minLength: {
                                 value: 6,
-                                message: "At least 6 characters required!"
+                                message: 'At least 6 characters required!'
                             }
                         })}
                         placeholder="Password"
@@ -57,13 +61,17 @@ const AuthForm: FunctionComponent = () => {
                     />
 
                     <div className="mt-5 mb-1 text-center">
-                        <Button onClick={() => setType("login")}>
+                        <Button
+                            onClick={() => setType('login')}
+                            disabled={isLoading}
+                        >
                             Login
                         </Button>
                     </div>
                     <button
                         className={styles.register}
-                        onClick={() => setType("register")}
+                        onClick={() => setType('register')}
+                        disabled={isLoading}
                     >
                         Register
                     </button>
