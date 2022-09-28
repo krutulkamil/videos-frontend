0363cc6221c59cf0ad17af70d8d013ac20a18a46
krutulkamil <krutulkamil@onet.pl>
2022-09-28 20:12:02 +0200

    store profile api

---
+++ app/store/api/api.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { API_URL } from '../../api/axios';
+import { TypeRootState } from '@/store/store';
+import { IUser } from '@/types/user.interface';
+import { USER } from '@/services/user.service';
+
+export const api = createApi({
+    reducerPath: 'api',
+    tagTypes: ['Video', 'Profile'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: API_URL,
+        prepareHeaders: (headers, { getState }) => {
+            const token = (getState() as TypeRootState).auth.accessToken;
+
+            if (token) headers.set('Authorization', `Bearer ${token}`);
+
+            return headers;
+        }
+    }),
+    endpoints: builder => ({
+        getProfile: builder.query<IUser, any>({
+            query: () => `${USER}/profile`,
+            providesTags: () => [{ type: 'Profile' }]
+        })
+    })
+});
