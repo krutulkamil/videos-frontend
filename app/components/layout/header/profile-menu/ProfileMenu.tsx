e7db3ee37c93dcd91e896c81d7c416da61e78deb
krutulkamil <krutulkamil@onet.pl>
2022-10-01 14:57:45 +0200

    feat: ProfileMenu dropdown + styles

--- app/components/layout/header/profile-menu/ProfileMenu.tsx
+++ app/components/layout/header/profile-menu/ProfileMenu.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from 'react';
+import Image from 'next/image';
 import styles from './ProfileMenu.module.scss';
 import { useAuth } from '../../../../../hooks/useAuth';
 import { api } from '@/store/api/api';
+import { useOutside } from '../../../../../hooks/useOutside';
+import { useActions } from '../../../../../hooks/useActions';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
+import Link from 'next/link';
 
 const ProfileMenu: FunctionComponent = () => {
     const { user } = useAuth();
@@ -10,10 +15,46 @@ const ProfileMenu: FunctionComponent = () => {
         skip: !user
     });
 
+    const { isShow, setIsShow, ref } = useOutside(false);
+
+    const { logout } = useActions();
+
     if (isLoading) return null;
 
     return (
-        <div>{data?.name}</div>
+        <div ref={ref} className={styles.wrapper}>
+            <button onClick={() => setIsShow(!isShow)}>
+                <Image
+                    src={data?.avatarPath || ''}
+                    alt={data?.name}
+                    width={40}
+                    height={40}
+                    priority
+                />
+                <span className={styles.name}>{data?.name}</span>
+                {isShow ? <GoChevronUp/> : <GoChevronDown/>}
+            </button>
+
+            {isShow && (
+                <div className={styles['profile-menu']}>
+                    <ul>
+                        <li>
+                            <Link href={`/c/${user?.id}`}>
+                                <a>My channel</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/studio'}>
+                                <a>Studio</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <button onClick={logout}>Logout</button>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
     );
 };
 
