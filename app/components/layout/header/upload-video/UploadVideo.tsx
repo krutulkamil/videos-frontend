45e91e8fee33655a4fe35fd92100c342abc63168
krutulkamil <krutulkamil@onet.pl>
2022-10-01 15:11:52 +0200

    feat: UploadVideo

--- app/components/layout/header/upload-video/UploadVideo.tsx
+++ app/components/layout/header/upload-video/UploadVideo.tsx
@@ -1,9 +1,33 @@
-import { FunctionComponent } from "react";
-import styles from "./UploadVideo.module.scss";
+import { FunctionComponent, useState } from 'react';
+import styles from './UploadVideo.module.scss';
+import stylesIcon from '../icons-right/IconsRight.module.scss';
+import { videoApi } from '@/store/api/video.api';
+import { HiUpload } from 'react-icons/hi';
 
 const UploadVideo: FunctionComponent = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [videoId, setVideoId] = useState<number>(0);
+
+    const [createVideo, { isLoading }] = videoApi.useCreateVideoMutation();
+
     return (
-        <div>Upload Video</div>
+        <>
+            <button
+                className={stylesIcon.button}
+                disabled={isLoading}
+                onClick={() => {
+                    createVideo()
+                        .unwrap()
+                        .then(id => {
+                        setVideoId(+id);
+                        setIsOpen(true);
+                    })
+                }}
+            >
+                <HiUpload/>
+            </button>
+            {/*<UploadModal isOpen={isOpen} setIsOpen={setIsOpen} videoId={videoId} />*/}
+        </>
     );
 };
 
